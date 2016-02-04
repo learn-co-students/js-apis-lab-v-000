3e5adb84d3f2b9b672715c7a3e5011137ae56b4e
Carleton DiLeo <cbdileo@gmail.com>
2016-02-04 09:40:25 -0800

    Added a hint to help with setting the correct headers

---
+++ js/github_client.js
@@ -0,0 +1,16 @@
+//define functions here
+var createGist = function(file_name, content, description, token){
+
+};
+
+var myGists = function (username, token){
+
+};
+
+var bindCreateButton = function() {
+  // call functions here
+
+};
+
+$(document).ready(function(){
+});
