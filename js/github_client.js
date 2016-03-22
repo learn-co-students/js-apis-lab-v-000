1306a9cf4b51f78c5656e231170769e6a5fddb72
pajamaw <pjpeterww@gmail.com>
2016-03-22 10:31:02 -0400

    Fixed event

--- js/github_client.js
+++ js/github_client.js
@@ -11,9 +11,9 @@ $.ajax({
   url: 'https://api.github.com/gists',
   type: 'POST',
   DataType: 'json',
-  headers: {
-    Authorization: token
-  }, 
+  beforeSend: function(xhr) {
+      xhr.setRequestHeader("Authorization", "token " + token);
+    }, 
   data: JSON.stringify(gistData)
 }).done(function(response){
   myGists(response.owner.login, token);
@@ -23,35 +23,33 @@ $.ajax({
 
 var myGists = function (username, token){
   $.ajax({
-  url: 'https://api.github.com/users/' +username+ '/gists',
-  type: 'GET',
-  DataType: 'json'
-
-}).done(function(gistResponse){
-
+    url: 'https://api.github.com/users/' +username+ '/gists',
+    type: 'GET',
+    dataType: 'jsonp'
+  }).done(function(gistResponse){
   $('ol.myGists').html('');
+
   $.each(gistResponse.data, function(index, gist){
 
-    var linkTag = $('<a>').attr('href', gist.html_url).text(gist.description);
+    var link = $('<a>').attr('href', gist.html_url).text(gist.description);
 
-    var gistHtml = $('<li>').append(linkTag);
+    var listItem = $('<li>').append(link);
 
-    $('ol.myGists').append(gistHtml);
+    $('ol.myGists').append(listItem);
   })
 
 });
 };
 
 var bindCreateButton = function (){
-  $('#submit').click(function() {
-    var username = $('#username').val();
+  $('#createGist').submit(function(event) {
     var token = $('#token').val();
     var file_name = $('#file_name').val();
     var description = $('#description').val();
     var content = $('#content').val();
 
+    event.preventDefault();
     createGist(file_name, content, description, token);
-    myGists(username, token);
     });
   };
 
@@ -59,4 +57,3 @@ $(document).ready(function(){
 
   bindCreateButton();
 });
-   // Authorization: "2cf42b54bbfb9c67ac9306d1862a499a46f1efad"
