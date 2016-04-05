6755df3f1980a9047b01a41bddc6b9092b706e27
zachnewburgh <zach.newburgh@gmail.com>
2016-04-05 08:28:09 -0400

    Done.

--- js/github_client.js
+++ js/github_client.js
@@ -1,16 +1,55 @@
+
+// var token = db410c6cd187a7b37b18b9d7395da98c39cca62a;
 //define functions here
+
 var createGist = function(file_name, content, description, token){
+  
+  var data = {
+    'public': true,
+    'description': description,
+    'files': {}
+  };
+
+  data['files'][file_name] = {
+    'content': content
+  };
+
+  $.ajax({
+    type: 'POST',
+    url: 'https://api.github.com/gists',
+    data: JSON.stringify(data),
+    dataType: 'json',
+    beforeSend: function(xhr) {
+      xhr.setRequestHeader('Authorization', 'token' + token);
+    },
+  }).done(function(response) {
+    myGists(response.owner.login, token);
+  })
 
 };
 
 var myGists = function (username, token){
 
+  $.get('https://api.github.com/users/' + username + '/gists', function(data) {
+    $('#gists').html('');
+    data.forEach(function(gist) {
+      $('#gists').append('<li><a href="' + gist.html_url + '">' + gist.description + '</a></li>');
+    });
+  });
+
 };
 
 var bindCreateButton = function() {
-  // call functions here
-
+  
+  $('#create').click(function() {
+    var file_name = $('#file_name').val();
+    var content = $('#content').val();
+    var description = $('#description').val();
+    var token = $('#token').val();
+    createGist(file_name, content, description, token);
+  });
 };
 
 $(document).ready(function(){
+  bindCreateButton();
 });
