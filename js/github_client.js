f1dc1582c22ba85693504208a7df918f8d77e598
authorbeard <msnolan@gmail.com>
2016-04-28 20:02:27 -0400

    testing gitignore

--- js/github_client.js
+++ js/github_client.js
@@ -1,10 +1,22 @@
 //define functions here
+
+var defaultUrl = 'https://api.github.com/'
+
 var createGist = function(file_name, content, description, token){
 
 };
 
 var myGists = function (username, token){
-
+  $.ajax({
+    url: defaultUrl + "/users" + username + "/gists",
+    type: 'GET',
+    dataType: 'json',
+    headers: {
+      Authorization: "token" + token
+    }
+  }).done(function(gists){
+    console.log("Got 'em")
+  })
 };
 
 var bindCreateButton = function() {
