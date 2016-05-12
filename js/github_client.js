b4d4a0944150bef53dd399204a8573fe8135ae9a
fiveinfinity <jlachance1@gmail.com>
2016-05-12 13:51:15 -0500

    jasmine tests passing for GET and POST API requests

--- js/github_client.js
+++ js/github_client.js
@@ -1,16 +1,50 @@
-//define functions here
+//var token = e5c9cc3828f5366e909fc4934544844302981881;
+
 var createGist = function(file_name, content, description, token){
 
-};
+  var data = {
+    'public':   true,
+    'description': description,
+    'files': {}
+  };
 
-var myGists = function (username, token){
+  data['files'][file_name] = {
+    'content': content
+  };
 
-};
+  $.ajax({
+    url: "https://api.github.com/gists",
+    type: "POST",
+    data: JSON.stringify(data),
+    dataType: 'json',
+    headers: {
+      Authorization: "e5c9cc3828f5366e909fc4934544844302981881"
+    }
+  }).done(function(response) {
+    myGists(response.owner.login, token);
+  });
+}
+
+var myGists = function(username, token){
+  $.ajax({
+    url: "https://api.github.com//users/" + username + "/gists",
+    type: 'GET',
+    dataType: 'jsonp'
+  }).done(function(gist) {
+    $("#gist-results").append(gist);
+  });
+}
 
 var bindCreateButton = function() {
-  // call functions here
-
-};
+  $("#gistme").click(function() {
+    var file_name = $("#file_name").val();
+    var content = $("#content").val();
+    var description = $("#description").val();
+    var token = $("#token").val();
+    createGist(file_name, content, description, token);
+  });
+}
 
 $(document).ready(function(){
+  bindCreateButton();
 });
