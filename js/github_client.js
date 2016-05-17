7331f6e9cef59cc8bc6c4d2a885128594b094db4
genericlady <yiannakokalas@gmail.com>
2016-05-17 14:40:39 -0400

    foobar

--- js/github_client.js
+++ js/github_client.js
@@ -21,16 +21,36 @@ var createGist = function(file_name, content, description, token){
 };
 
 var myGists = function (username, token){
+  var githubURL = 'https://api.github.com/' + username + '/gists'
+
   $.ajax( {
-    
+    url: githubURL,
+    type: 'GET',
+    dataType: 'JSON',
+    headers: {
+      'Authorization': token
+    },
+    success: function ( response ) {
+      console.log(response);
+    }
   } );
-  console.log(username + token);
 };
 
 var bindCreateButton = function() {
-  // call functions here
+  var newGistParameters = {};
+  $( 'button' ).on( 'click', function ( event ) {
+    // createGist()
+    var fields = $( ':input' ).serializeArray();
+    // var formData = $( 'form' ).serializeArray();
+    jQuery.each( fields, function ( i, field ) {
+      newGistParameters[field['name']] = field['value'];
+    })
+    createGist( newGistParameters );
+    event.preventDefault();
 
+  } );
 };
 
 $(document).ready(function(){
+  bindCreateButton();
 });
