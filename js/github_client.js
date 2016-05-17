c6f795430aeca629f948bd8d0cacb1c2ab2b924b
genericlady <yiannakokalas@gmail.com>
2016-05-17 12:33:31 -0400

    hey

--- js/github_client.js
+++ js/github_client.js
@@ -1,10 +1,30 @@
 //define functions here
 var createGist = function(file_name, content, description, token){
+  var url = 'https://api.github.com/gists'
 
+  var data = {
+    'public':   true,
+    'description': description,
+    'files': {
+      [file_name]: {
+        'content': content
+      }
+    }
+  }
+
+  $.ajax( {
+    type: 'POST',
+    url: url,
+    data: ( JSON.stringify( data ) ),
+    dataType: 'json',
+  } );
 };
 
 var myGists = function (username, token){
-
+  $.ajax( {
+    
+  } );
+  console.log(username + token);
 };
 
 var bindCreateButton = function() {
