af176056c244bda734f26c5ce62e1145d4f31c72
genericlady <yiannakokalas@gmail.com>
2016-05-18 17:03:04 -0400

    Done.

--- js/github_client.js
+++ js/github_client.js
@@ -1,15 +1,121 @@
-var createGist = function(file_name, content, description, token) {
-  var url = 'https://api.github.com/gists'
+// var createGist = function(file_name, content, description, token) {
+//   var url = 'https://api.github.com/gists'
+//
+//   var data = {
+//     'public':   true,
+//     'description': description,
+//     'files': {
+//       [file_name]: {
+//         'content': content
+//       }
+//     }
+//   }
+//
+//   $.ajax({
+//     url: 'https://api.github.com/gists',
+//     type: 'POST',
+//     dataType: 'json',
+//     beforeSend: function(xhr) {
+//       xhr.setRequestHeader("Authorization", "token " + token);
+//     },
+//     data: JSON.stringify(data)
+//   }).done(function(response) {
+//     myGists(response.owner.login, token);
+//   });
+//
+// };
+//
+// //   $.ajax({
+// //     url: url,
+// //     type: 'POST',
+// //     dataType: 'json',
+// //     beforeSend: function(xhr) {
+// //       xhr.setRequestHeader("Authorization", "token " + token);
+// //     }
+// //     data: (JSON.stringify(data)),
+// //     success: function(response) {
+// //       myGists(response.owner.login, token);
+// //     }
+// //   } );
+// //   myGists('fake login', token);
+// //   // myGists( 'genericlady', token );
+// // };
+//
+// // var myGists = function(username, token) {
+// //   var githubURL = 'https://api.github.com/users/' + username + '/gists'
+// //
+// //   $.ajax({
+// //     url: githubURL,
+// //     type: 'GET',
+// //     dataType: 'jsonp',
+// //     headers: {
+// //       'Authorization': 'token ' + token
+// //     },
+// //     success: function(gists) {
+// //       $('#gists').html('');
+// //
+// //       jQuery.each(gists.data, function(index, gist) {
+// //         var link = $('<a>')
+// //           .attr('href', gist.html_url)
+// //           .text(gist.description);
+// //
+// //         var listItem = $('li')
+// //           .append(link);
+// //
+// //         $('#gists').append(listItem);
+// //       })
+// //     }
+// //   } );
+// // };
+// var myGists = function (username, token){
+//   $.ajax({
+//     url: 'https://api.github.com/users/' + username + '/gists',
+//     type: 'GET',
+//     dataType: 'jsonp'
+//   }).done(function(gists) {
+//     $('#myGists').html('');
+//
+//     $.each(gists.data, function(index, gist) {
+//       var link = $('<a>')
+//         .attr('href', gist.html_url)
+//         .text(gist.description);
+//
+//       var listItem = $('<li>')
+//         .append(link);
+//
+//       $('#myGists').append(listItem);
+//     })
+//   });
+// };
+// var bindCreateButton = function() {
+//   var newGistParameters = {};
+//
+//   $('button').on('click', function(event) {
+//     var fields = $(':input').serializeArray();
+//
+//     jQuery.each(fields, function(i, field) {
+//       newGistParameters[field['name']] = field['value'];
+//     } )
+//     createGist(newGistParameters);
+//
+//   });
+// };
+//
+// $(document).ready(function() {
+//   bindCreateButton();
+// });
 
+//define functions here
+var createGist = function(file_name, content, description, token){
   var data = {
     'public':   true,
     'description': description,
-    'files': {
-      [file_name]: {
-        'content': content
-      }
-    }
-  }
+    'files': {}
+  };
+
+  data['files'][file_name] = {
+    'content': content
+  };
 
   $.ajax({
     url: 'https://api.github.com/gists',
@@ -22,51 +128,8 @@ var createGist = function(file_name, content, description, token) {
   }).done(function(response) {
     myGists(response.owner.login, token);
   });
-
 };
 
-//   $.ajax({
-//     url: url,
-//     type: 'POST',
-//     dataType: 'json',
-//     beforeSend: function(xhr) {
-//       xhr.setRequestHeader("Authorization", "token " + token);
-//     }
-//     data: (JSON.stringify(data)),
-//     success: function(response) {
-//       myGists(response.owner.login, token);
-//     }
-//   } );
-//   myGists('fake login', token);
-//   // myGists( 'genericlady', token );
-// };
-
-// var myGists = function(username, token) {
-//   var githubURL = 'https://api.github.com/users/' + username + '/gists'
-//
-//   $.ajax({
-//     url: githubURL,
-//     type: 'GET',
-//     dataType: 'jsonp',
-//     headers: {
-//       'Authorization': 'token ' + token
-//     },
-//     success: function(gists) {
-//       $('#gists').html('');
-//
-//       jQuery.each(gists.data, function(index, gist) {
-//         var link = $('<a>')
-//           .attr('href', gist.html_url)
-//           .text(gist.description);
-//
-//         var listItem = $('li')
-//           .append(link);
-//
-//         $('#gists').append(listItem);
-//       })
-//     }
-//   } );
-// };
 var myGists = function (username, token){
   $.ajax({
     url: 'https://api.github.com/users/' + username + '/gists',
@@ -87,20 +150,19 @@ var myGists = function (username, token){
     })
   });
 };
+
 var bindCreateButton = function() {
-  var newGistParameters = {};
+  // call functions here
+  $('#create').click(function() {
+    var token = $('#token').val();
+    var file_name = $('#file_name').val();
+    var content = $('#content').val();
+    var description = $('#description').val();
 
-  $('button').on('click', function(event) {
-    var fields = $(':input').serializeArray();
-
-    jQuery.each(fields, function(i, field) {
-      newGistParameters[field['name']] = field['value'];
-    } )
-    createGist(newGistParameters);
-
+    createGist(file_name, content, description, token);
   });
 };
 
-$(document).ready(function() {
+$(document).ready(function(){
   bindCreateButton();
 });
