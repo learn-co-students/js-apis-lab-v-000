1011c40f24cae7bc9af1c63b34c672c4cddbe58f
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-07-14 21:28:32 +0000

    set up the basic UI and 2 out of 3 functions

--- js/github_client.js
+++ js/github_client.js
@@ -4,13 +4,36 @@ var createGist = function(file_name, content, description, token){
 };
 
 var myGists = function (username, token){
-
+  var url = "https://api.github.com/users/" + username + "/gists";
+  $.ajax({
+    url: url,
+    type: 'GET',
+    dataType: 'json',
+    headers: {
+      Authorization: 'token ' + token
+    }
+  }).success(function(data){
+    console.log(data);
+  });
 };
 
 var bindCreateButton = function() {
   // call functions here
-
+  $("input#submit").on("click", function(event){
+    var name = $("input#username").val();
+    var token = $("input#token").val();
+    var fileName = $("input#file-name").val();
+    var content = $("textarea#content").val();
+    var description = $("input#description").val();
+    console.log("user: " + name + "\n filename: " + fileName + "\n content: " + content + "\n description: " + description)
+    myGists(name, token);
+    if(fileName.length > 1 && content.length > 1 && description.length > 1){
+      createGist(fileName, content, description, token);
+    }
+    event.preventDefault();
+  });
 };
 
 $(document).ready(function(){
+  bindCreateButton();
 });
