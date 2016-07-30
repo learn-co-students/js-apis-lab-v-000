b15dac06c48380b0321d40669ca09e02c7afac9b
Jason Casares <jasonc@csdrafting.com>
2016-07-30 18:58:18 +0000

    Done.

--- js/github_client.js
+++ js/github_client.js
@@ -1,6 +1,6 @@
 //define functions here
 var createGist = function(file_name, content, description, token){
-
+  token: da5edf923f8aefd5d337c0b9a9e6902b7c166363
 };
 
 var myGists = function (username, token){
