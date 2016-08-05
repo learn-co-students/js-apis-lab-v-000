d6c77bdb399a91b041cda06fbcc5a654be1a4529
reichardn <reichardn@gmail.com>
2016-08-04 21:33:02 -0400

    Done.

--- js/github_client.js
+++ js/github_client.js
@@ -1,10 +1,53 @@
+// github token = 183e463f467342ce8c26424919376cecb0917a8b
+// https://api.github.com/users/reichardn/gists
+// https://api.github.com/gists
 //define functions here
-var createGist = function(file_name, content, description, token){
 
+
+
+var createGist = function(file_name, content, description, token){
+  $.ajax({
+    type: 'POST',
+    url: 'https://api.github.com/gists',
+    data: {
+      "description": "the description for this gist",
+      "public": true,
+      "files": {
+        "file1.txt": {
+          "content": "String file contents"
+        }
+      }
+    },
+    headers: {
+      Authorization: `token ${token}`
+    },
+    success: function(msg){
+      console.log('successfully created');
+    },
+    error: function(e) {
+      console.log(e)
+    }
+  });
 };
 
 var myGists = function (username, token){
-
+  var json;
+  $.ajax({
+    url: `https://api.github.com/users/${username}/gists`,
+    type: 'GET',
+    dataType: 'json',
+    async: false,
+    headers: {
+      Authorization: `token ${token}`
+    },
+    success: function(response) {
+      json = response;
+    },
+    error: function (error) {
+      console.log('error');
+    }
+  });
+  return json;
 };
 
 var bindCreateButton = function() {
