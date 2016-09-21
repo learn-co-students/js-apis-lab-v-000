3fca139cad99b479993c6c089d2e6a03ced0e606
Joseph Lim <joseph.d.lim@gmail.com>
2016-09-21 16:38:21 -0400

    Done.

--- js/github_client.js
+++ js/github_client.js
@@ -1,16 +1,54 @@
 //define functions here
-var createGist = function(file_name, content, description, token){
+var tkn = "a3bef0996a87cd2aa8db7ec6ffb80adb13a0671c"
 
+var createGist = function(file_name, content, description, token){
+  var files = {[file_name] : {'content': content}}
+  $.ajax({
+    url: 'https://api.github.com/gists',
+    type: 'POST',
+    dataType: 'json',
+    data: JSON.stringify({'public': true, 'description': description, files }),
+    headers: {
+      Authorization: token
+    }
+  }).done(function(response) {
+   myGists(response.owner.login, token);
+ });
 };
 
-var myGists = function (username, token){
 
+var myGists = function (username, token){
+  $.ajax({
+    url: 'https://api.github.com/users/' + username + '/gists',
+    type: 'GET',
+    dataType: 'jsonp'
+  }).done(function(gists) {
+    $('#myGists').html('');
+
+    $.each(gists.data, function(index, gist) {
+      var link = $('<a>')
+        .attr('href', gist.html_url)
+        .text(gist.description);
+
+      var listItem = $('<li>')
+        .append(link);
+
+      $('#myGists').append(listItem);
+    })
+  });
 };
 
 var bindCreateButton = function() {
-  // call functions here
+  $('#create').click(function() {
+    var token = $('#token').val();
+    var file_name = $('#file_name').val();
+    var content = $('#content').val();
+    var description = $('#description').val();
 
-};
+    createGist(file_name, content, description, token);
+  });
+}
 
 $(document).ready(function(){
+  bindCreateButton();
 });
