77b9e94511d3a31b38242dda5392da12c0d57f81
jbok4 <jbok4@aol.com>
2016-10-28 16:59:25 -0400

    initial setup

--- js/github_client.js
+++ js/github_client.js
@@ -1,6 +1,27 @@
 //define functions here
-var createGist = function(file_name, content, description, token){
+// var token = 64617174385df2078fd7e2115c8f5a8ae2b2b593;
 
+var createGist = function(file_name, content, description, token){
+  var data = {
+    "description": description,
+    "public": true,
+    "files": {}
+  }
+
+  data.files[file_name] = {
+    "content": content
+  }
+  $.ajax({
+    url: 'https://api.github.com/gists',
+    type: 'POST',
+    dataType: 'json',
+    headers: {
+      Authorization: "64617174385df2078fd7e2115c8f5a8ae2b2b593"
+    }
+    data: JSON.stringify(data)
+  }).done(function(response) {
+    myGists(response.owner.login, token)
+  })
 };
 
 var myGists = function (username, token){
@@ -13,4 +34,9 @@ var bindCreateButton = function() {
 };
 
 $(document).ready(function(){
+   bindCreateButton();
 });
+
+
+// Tokens you have generated that can be used to access the GitHub API.
+// gist 64617174385df2078fd7e2115c8f5a8ae2b2b593
