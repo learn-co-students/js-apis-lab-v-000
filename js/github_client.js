4325f6de038596c6ed4a80866073630a0e7ea9bc
tpstar <hanlee12@gmail.com>
2016-11-03 08:11:39 -0400

    ajax post

--- js/github_client.js
+++ js/github_client.js
@@ -1,16 +1,48 @@
 //define functions here
 var createGist = function(file_name, content, description, token){
 
+  var dataObject = {
+    "description": description,
+    "public": true,
+    "files": {
+      file_name: {
+        "content": content
+      }
+    }
+  }
+
+  $.ajax({
+    url: 'https://api.github.com/gists',
+    type: 'POST',
+    dataType: 'json',
+    data: JSON.stringify(dataObject),
+    headers: {
+      Authorization: token
+    },
+  }).done(function(result) {
+    myGists(result, token)
+  });
 };
 
 var myGists = function (username, token){
-
+  alert("got to myGists");
+  // $.ajax({
+  //
+  // }).done(functions(result) {
+  //
+  // });
 };
 
 var bindCreateButton = function() {
   // call functions here
+  var file_name = $('#filename').val();
+  var content = $('#content').val();
+  var description = $('#description').val();
+  var token = $('#token').val();
+  createGist(file_name, content, description, token);
 
 };
 
 $(document).ready(function(){
+  bindCreateButton();
 });
