c9eacfb5923c27c21ccd9c479b10d9d3c709cd6c
Samuel Lair <lair001@gmail.com>
2016-11-12 01:17:00 -0500

    Done.

--- js/github_client.js
+++ js/github_client.js
@@ -1,10 +1,42 @@
 //define functions here
 var createGist = function(file_name, content, description, token){
-
+	var dataHash = {
+	  	'description': description,
+	  	'public': true,
+	  	'files': {}
+	  };
+	dataHash['files'][file_name] = { 'content': content }
+	var requestHash = {
+	  url: 'https://api.github.com/gists',
+	  type: 'POST',
+	  dataType: 'json',
+	  headers: {
+	    Authorization: "token " + token
+	  },
+	  data: JSON.stringify(dataHash)
+	};
+	$.ajax(requestHash).done(function(response) {
+		myGists(response.owner.login, token);
+	}).fail(function(error) {
+		console.log("I am error!");
+		console.log(error);
+	});
 };
 
 var myGists = function (username, token){
-
+	$.ajax({
+	  url: 'https://api.github.com/users/' + username + '/gists',
+	  type: 'GET',
+	  dataType: 'json',
+	  headers: {
+	    Authorization: "token " + token
+	  }
+	}).done(function(gists) {
+		return gists;
+	}).fail(function(error) {
+		console.log("I am error!");
+		console.log(error);
+	});
 };
 
 var bindCreateButton = function() {
@@ -13,4 +45,5 @@ var bindCreateButton = function() {
 };
 
 $(document).ready(function(){
+	bindCreateButton();
 });
