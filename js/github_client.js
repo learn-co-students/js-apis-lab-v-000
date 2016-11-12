f7fe7dd6bc5b1172ee62407483a6742edec9022f
Samuel Lair <lair001@gmail.com>
2016-11-12 02:50:09 -0500

    Done.

--- js/github_client.js
+++ js/github_client.js
@@ -1,14 +1,20 @@
 //define functions here
 var createGist = function(file_name, content, description, token){
-	console.log("are we even hitting createGist?");
+	console.log("createGist called?");
+	console.log("file name?");
+  	console.log(file_name);
+  	console.log("content?");
+  	console.log(content);
+  	console.log("description?");
+  	console.log(description);
+  	console.log("token?");
+  	console.log(token);
 	var dataHash = {
 	  	'description': description,
 	  	'public': true,
 	  	'files': {}
 	  };
 	dataHash['files'][file_name] = { 'content': content }
-	console.log("Do we have a dataHash?");
-	console.log(dataHash);
 	var requestHash = {
 	  url: 'https://api.github.com/gists',
 	  type: 'POST',
@@ -18,16 +24,12 @@ var createGist = function(file_name, content, description, token){
 	  },
 	  data: JSON.stringify(dataHash)
 	};
-	console.log("Do we have a requestHash?");
+	console.log("requestHash:");
 	console.log(requestHash);
 	$.ajax(requestHash).done(function(response) {
-		console.log("Does the createGist ajax request succeed?");
-		console.log("Does myGists exist in createGist?");
-		console.log(myGists);
-		var gists = myGists(response.owner.login, token);
-		console.log("Are the Gists in createGist?")
-		console.log(gists);
-		return gists;
+		console.log("does createGist succeed?");
+		console.log(response);
+		myGists(response.owner.login, token);
 	}).fail(function(error) {
 		console.log("I am error!");
 		console.log(error);
@@ -35,6 +37,7 @@ var createGist = function(file_name, content, description, token){
 };
 
 var myGists = function (username, token){
+	console.log("is myGists ever called?")
 	$.ajax({
 	  url: 'https://api.github.com/users/' + username + '/gists',
 	  type: 'GET',
@@ -43,16 +46,30 @@ var myGists = function (username, token){
 	    Authorization: "token " + token
 	  }
 	}).done(function(gists) {
-		return gists;
+		console.log("does myGists succeed?");
+		console.log(gists);
+		displayGists(gists);
 	}).fail(function(error) {
 		console.log("I am error!");
 		console.log(error);
 	});
 };
 
+var displayGists = function(gists) {
+	console.log("displayGists called?");
+	var html = ""
+	gists.forEach(function(gist){
+  		html += "<a href='" + gist.url + "'><li>" + gist.description + "</li></a>";
+  	});
+  	console.log("Html:")
+  	console.log(html)
+  	$("#gists").html(html);
+};
+
 var bindCreateButton = function() {
   // call functions here
   $('#create-gist').on("click", function(event){
+  	event.preventDefault();
   	var file_name = $('#file_name').val();
   	console.log("file name?");
   	console.log(file_name);
@@ -65,20 +82,7 @@ var bindCreateButton = function() {
   	var token = $('#token').val();
   	console.log("token?");
   	console.log(token);
-  	console.log("Does createGist exist in bindCreateButton?")
-  	console.log(createGist);
-  	console.log("Let's see what a createGist call returns");
-  	console.log(createGist(file_name, content, description, token));
-  	var gists = createGist(file_name, content, description, token);
-  	var html = "";
-  	console.log("Do the gists now exist in bindCreateButton?")
-  	console.log(gists);
-  	gists.forEach(function(gist){
-  		console.log(gist);
-  		html += "<a href='" + gist.url + "'><li>" + gist.description + "</li></a>";
-  	});
-  	console.log(html);
-  	$("#gists").html(html);
+  	createGist(file_name, content, description, token);
   });
 };
 
