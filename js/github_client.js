5a6afeeec0f4f933a5c535188a72bb214d6db9b9
lenn hypolite <lennhydesign@gmail.com>
2016-11-15 13:22:18 -0500

    Done

--- js/github_client.js
+++ js/github_client.js
@@ -1,50 +1,48 @@
-
-
-//define functions here
-
-// returns the gists for a user.
+// creates and returns the gists of a user.
 var createGist = function(file_name, content, description, token){
+  var data = {
+    description: description,
+    "public": true,
+    "files": {}
+  };
+  // Below file_name is being set to the file_name parameter being passed in above
+  data["files"][file_name] = {"content": content};
   $.ajax({
      url: "https://api.github.com/gists",
      type: "POST",
-     data: JSON.stringify(
-       {
-         description: description,
-         "files": {
-           file_name: {
-             "content": content
-           }
-         }
-       }
-     )
+     dataType: 'json',
+     beforeSend: function (request){
+           request.setRequestHeader("Authority", token);
+     },
+     data: JSON.stringify(data)
   })
   .done(function(response){
     console.log(response);
+    myGists(response.owner.login, token);
   })
   .fail(function(error){
-    console.log(error + "crap")
+    console.log(error.responseText)
   })
 };
 
-
+// Get user gists
 var myGists = function (username, token){
-  var username="lennhy";
   var myUrl = "https://api.github.com/users/"+username+"/gists"
   $.ajax({
     url: myUrl,
     type: "GET"
 
   }).done(function(gists){
-      // myGists(username, token)
       $.each(gists, function(i, gist){
         console.log(gist);
       });
   });
 };
 
+// click event triggers gist creation
 var bindCreateButton = function() {
   var token = $('#token').val();
-  var file_name = $('#name').val();
+  var file_name = $('#file_name').val();
   var description = $('#description').val();
   var content = $('#content').val();
 
@@ -55,6 +53,5 @@ var bindCreateButton = function() {
 
 
 $(document).ready(function(){
-  // myGists();
   bindCreateButton();
 });
