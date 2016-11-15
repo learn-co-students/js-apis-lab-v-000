70d7d8cea06df9eadd79ce7e8b0ee6516ab12636
lenn hypolite <lennhydesign@gmail.com>
2016-11-15 00:03:32 -0500

    initial commit

--- js/github_client.js
+++ js/github_client.js
@@ -1,16 +1,60 @@
-//define functions here
-var createGist = function(file_name, content, description, token){
 
+
+//define functions here
+
+// returns the gists for a user.
+var createGist = function(file_name, content, description, token){
+  $.ajax({
+     url: "https://api.github.com/gists",
+     type: "POST",
+     data: JSON.stringify(
+       {
+         description: description,
+         "files": {
+           file_name: {
+             "content": content
+           }
+         }
+       }
+     )
+  })
+  .done(function(response){
+    console.log(response);
+  })
+  .fail(function(error){
+    console.log(error + "crap")
+  })
 };
 
-var myGists = function (username, token){
 
+var myGists = function (username, token){
+  var username="lennhy";
+  var myUrl = "https://api.github.com/users/"+username+"/gists"
+  $.ajax({
+    url: myUrl,
+    type: "GET"
+
+  }).done(function(gists){
+      // myGists(username, token)
+      $.each(gists, function(i, gist){
+        console.log(gist);
+      });
+  });
 };
 
 var bindCreateButton = function() {
-  // call functions here
+  var token = $('#token').val();
+  var file_name = $('#name').val();
+  var description = $('#description').val();
+  var content = $('#content').val();
 
+  $('#create').click(function() {
+    createGist(file_name, content, description, token);
+  });
 };
 
+
 $(document).ready(function(){
+  // myGists();
+  bindCreateButton();
 });
