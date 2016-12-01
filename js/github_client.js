0ab1f47b2cdcb394538843b19898d36fe025751a
bedle88 <robey.mc@gmail.com>
2016-12-01 14:38:48 -0500

    done

--- js/github_client.js
+++ js/github_client.js
@@ -1,16 +1,58 @@
-//define functions here
-var createGist = function(file_name, content, description, token){
-
-};
-
-var myGists = function (username, token){
-
-};
+$(document).ready(function(){
+  bindCreateButton();
+});
 
 var bindCreateButton = function() {
-  // call functions here
+  $('#create').click(function() {
+    var token = $('#token').val();
+    var fileName = $('#file_name').val();
+    var description = $('#description').val();
+    var content = $('#content').val();
 
+    createGist(fileName, content, description, token);
+  })
 };
 
-$(document).ready(function(){
-});
+var createGist = function(fileName, content, description, token) {
+  var data = {
+      public: true,
+      description: description,
+      files: {
+        // [fileName]: {
+        //   content: content
+        // }
+      }
+    };
+
+    data['files'][fileName] = {
+      content: content
+    };
+
+    $.ajax({
+      url: 'https://api.github.com/gists',
+      type: 'POST',
+      dataType: 'json',
+      beforeSend: function(xhr) {
+        xhr.setRequestHeader("Authorization", "token " + token);
+      },
+      data: JSON.stringify(data)
+    }).done(function(response) {
+      myGists(response.owner.login, token);
+    });
+  };
+
+var myGists = function (username, token) {
+  $.ajax({
+    url: 'https://api.github.com/users/' + username + '/gists',
+    type: 'GET',
+    dataType: 'jsonp'
+  }).done(function(gists){
+    $('#yourGists').html('');
+
+    $.each(gists.data, function(index, gist) {
+      var link = $('<a>').attr('href', gist.html_url).text(gist.description);
+      var listItem = $('<li>').append(link);
+      $('#yourGists').append(listItem);
+    })
+  })
+};
