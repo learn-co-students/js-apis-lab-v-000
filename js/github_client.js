cc4ffe9ca15f34e557a97a9c8b09ba592fd9340f
bshap93 <bshap93@gmail.com>
2016-12-26 18:22:00 -0700

    Done.

--- js/github_client.js
+++ js/github_client.js
@@ -1,15 +1,53 @@
 //define functions here
 var createGist = function(file_name, content, description, token){
+	var data = {
+		'public': true,
+		'description' : description,
+		'files': {}
+	};
+	data['files'][file_name] = {
+		'content': content
+	};
 
+	$.ajax({
+		url: "https://api.github.com/gists",
+		type: 'POST',
+		dataType: 'json',
+		data: JSON.stringify(data),
+		headers: {
+			Authorization: `token ${token}`
+		}
+	}).done(function(response) {
+		myGists(response.owner.login, token);
+	});
 };
 
 var myGists = function (username, token){
-
+	$.ajax({
+		url: "https://api.github.com/users/" + username + "/gists",
+		type: 'GET',
+		dataType: 'json',
+		headers: {
+			Authorization: `token ${token}`
+		}
+	}).done(function(gists) {
+		$.each(gists, function(index, gist) {
+			$('#myGists').append(`<li>
+				<a href="${gist.html_url}">${gist.description}</a>
+				</li>`
+			)
+		})
+	});
 };
 
 var bindCreateButton = function() {
   // call functions here
-
+	debugger
+	var token = $('#gistToken').val();
+	var file_name = $('#gistFileName').val();
+	var description = $('#gistDescription').val();
+	var contents = $('#gistContents').val();
+	createGist(file_name, contents, description, token);
 };
 
 $(document).ready(function(){
