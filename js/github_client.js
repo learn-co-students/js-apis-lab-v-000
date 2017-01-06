deccbf1e38151b88fa5f2bdffcece1df30e33a37
Yechiel K <chilikkal@gmail.com>
2017-01-05 22:58:55 -0500

    Done.

--- js/github_client.js
+++ js/github_client.js
@@ -1,10 +1,33 @@
 //define functions here
 var createGist = function(file_name, content, description, token){
-
+  $.ajax({
+    url: 'https://api.github.com/gists',
+    type: 'POST',
+    dataType: 'json',
+    body: {
+      description: description,
+      public: true,
+      files: {
+        file_name: {
+          content: content
+        }
+      }
+    },
+    headers: {
+      Authorization: token
+    }
+  })
 };
 
 var myGists = function (username, token){
-
+  $.ajax({
+    url: 'https://api.github.com/users/' + username + '/gists',
+    type: 'GET',
+    dataType: 'json',
+    headers: {
+      Authorization: token
+    }
+  })
 };
 
 var bindCreateButton = function() {
